f4553217aa9280ea1aa4ef68830b9ce13d25e8ca
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-03-18 13:45:35 -0300

    criação e implementação do Repository

--- contab-process-data/src/main/java/contab/process_data/dto/ProcessMonthlyRecord.java
+++
@@ -1,7 +0,0 @@
-package contab.process_data.dto;
-
-import java.time.LocalDateTime;
-
-public record ProcessMonthlyRecord(LocalDateTime init,
-                                   LocalDateTime finish) {
-}
--- contab-process-data/src/main/java/contab/process_data/persistence/repositories/SpotifyRepository.java
+++ contab-process-data/src/main/java/contab/process_data/persistence/repositories/SpotifyRepository.java
@@ -2,6 +2,7 @@ package contab.process_data.persistence.repositories;
 
 import contab.process_data.persistence.entities.ArtistsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,9 @@ import java.util.List;
 @Repository
 public interface SpotifyRepository extends JpaRepository<ArtistsEntity, Long> {
     List<ArtistsEntity> getAll();
+    List<ArtistsEntity> findByNameLike(String content);
+    List<ArtistsEntity> findByLastNameLike(String content);
+
+@NativeQuery(value = "select a.* from artists a where lastName = ?1")
+    List<ArtistsEntity> selectNativeQuery(String content);
 }
